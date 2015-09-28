9239c6081fd158a5a85b7fca2483c73eb747b0f2
raizeta <rai_zet@hotmail.com>
2015-09-29 01:11:29 +0700

    Setting KNPPaginatorBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new UsersBundle\UsersBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
